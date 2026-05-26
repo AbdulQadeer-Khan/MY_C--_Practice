68a9b3005023004aa4dc8bba159a5cf050cacae3
AbdulQadeer-Khan <abdulqadeeralam49@gmail.com>
2026-05-26 16:22:35 +0500

    corrected the logic of reversing an array using two pointer logic

--- function practice/function_ReverseArray.cpp
+++ function practice/function_ReverseArray.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
 using namespace std;
-int reverseArray(int arr[],int size);
+void reverseArray(int arr[],int size);
 
 int main (){
-    int arr[5], size;
+    int arr[100], size;
     cout<<"Enter size of the array \n";
     cin>>size;
     cout<<"Now enter "<<size<<" values in the array \n";
     for (int i=0;i<size;i++){
         cin>>arr[i];
     }
-    cout<<"revverse of the array is"<<reverseArray(arr,size)<<endl;
+    cout<<"revverse of the array is"<<endl;
+    reverseArray(arr,size);
+    for (int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
 
-int reverseArray(int arr[],int size){
-    int lastdigit;
-    int revarr[size];
-    for (int i=0;i<size;i++){
-        while(arr[i]>0){
-            lastdigit=arr[i]%10;
-            revarr[i]=(revarr[i]*10) + lastdigit;
-            arr[i]=arr[i]/10;
-        }
+void reverseArray(int arr[],int size){
+    int start=0;
+    int end=size-1;
+    
+    while (start<end){
+        int swap= arr[start];
+        arr[start] =arr[end];
+        arr[end]=swap;
         
+        start++;
+        end--;
+
     }
     
-    for (int i=0;i<size;i++){
-        return revarr[i];
-    }
 }
