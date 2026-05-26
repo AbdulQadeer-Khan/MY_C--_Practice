8f512dfd372209975b37585c0fa5db0421053413
AbdulQadeer-Khan <abdulqadeeralam49@gmail.com>
2026-05-26 21:09:46 +0500

    first class problem

---
+++ Class_1Practice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class rectangle{
+    private:
+    double length;
+    double width;
+
+    public:
+    rectangle(double l,double w){
+        length=l;
+        width=w;
+    }
+
+    double getArea(){
+        return length * width;
+    }
+    double getperimeter(){
+        return 2* (length + width);
+    }
+
+};
+int main(){
+    rectangle data(5.0,3.0);
+    cout<<"Area Of Rectangle is :"<<data.getArea()<<endl;
+    cout<<"perimeter of Rectangle is :"<<data.getperimeter()<<endl;
+}
---
+++ String_Practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Enter your full name"<<endl;
+    getline(cin,name);
+    
+    cout<<"your name is :";
+
+    cout<<name;
+}
